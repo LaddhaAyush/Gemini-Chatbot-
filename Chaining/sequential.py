2a6603c7f1ec9585a16d08a1402a3dc59c6436bb
“Laddha <ayushladdha123@gmail.com>
2025-06-19 12:08:06 +0530

    Chaining added

---
+++ Chaining/sequential.py
@@ -0,0 +1,37 @@
+
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint   
+
+
+load_dotenv()
+
+prompt1 = PromptTemplate(
+    template='Generate a detailed report on {topic}',
+    input_variables=['topic']
+)
+
+prompt2 = PromptTemplate(
+    template='Generate a 5 pointer summary from the following text \n {text}',
+    input_variables=['text']
+)
+
+llm = HuggingFaceEndpoint(
+    model="mistralai/Magistral-Small-2506",
+    task="text-generation"
+)
+model = ChatHuggingFace(
+    llm=llm,
+    model_kwargs={"temperature": 0.4}
+)
+
+parser = StrOutputParser()
+
+chain = prompt1 | model | parser | prompt2 | model | parser
+
+result = chain.invoke({'topic': 'Unemployment in India'})
+
+print(result)
+
+chain.get_graph().print_ascii()
